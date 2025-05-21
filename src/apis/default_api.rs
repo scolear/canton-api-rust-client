9b969d4198fff7d2560a77703fa1b9b6aec8db88
scolear <bobtera@gmail.com>
2025-05-21 11:13:03 +0200

    remove post packages

--- src/apis/default_api.rs
+++ src/apis/default_api.rs
@@ -2692,64 +2692,6 @@ pub async fn post_v2_interactive_submission_prepare(
     }
 }
 
-/// Upload a DAR to the participant node
-pub async fn post_v2_packages(
-    configuration: &configuration::Configuration,
-    body: std::path::PathBuf,
-) -> Result<serde_json::Value, Error<PostV2PackagesError>> {
-    // add a prefix to parameters to efficiently prevent name collisions
-    let p_body = body;
-
-    let uri_str = format!("{}/v2/packages", configuration.base_path);
-    let mut req_builder = configuration
-        .client
-        .request(reqwest::Method::POST, &uri_str);
-
-    if let Some(ref user_agent) = configuration.user_agent {
-        req_builder = req_builder.header(reqwest::header::USER_AGENT, user_agent.clone());
-    }
-    if let Some(ref token) = configuration.bearer_access_token {
-        req_builder = req_builder.bearer_auth(token.to_owned());
-    };
-    if let Some(ref apikey) = configuration.api_key {
-        let key = apikey.key.clone();
-        let value = match apikey.prefix {
-            Some(ref prefix) => format!("{} {}", prefix, key),
-            None => key,
-        };
-        req_builder = req_builder.header("Sec-WebSocket-Protocol", value);
-    };
-    req_builder = req_builder.body(p_body);
-
-    let req = req_builder.build()?;
-    let resp = configuration.client.execute(req).await?;
-
-    let status = resp.status();
-    let content_type = resp
-        .headers()
-        .get("content-type")
-        .and_then(|v| v.to_str().ok())
-        .unwrap_or("application/octet-stream");
-    let content_type = super::ContentType::from(content_type);
-
-    if !status.is_client_error() && !status.is_server_error() {
-        let content = resp.text().await?;
-        match content_type {
-            ContentType::Json => serde_json::from_str(&content).map_err(Error::from),
-            ContentType::Text => return Err(Error::from(serde_json::Error::custom("Received `text/plain` content type response that cannot be converted to `serde_json::Value`"))),
-            ContentType::Unsupported(unknown_type) => return Err(Error::from(serde_json::Error::custom(format!("Received `{unknown_type}` content type response that cannot be converted to `serde_json::Value`")))),
-        }
-    } else {
-        let content = resp.text().await?;
-        let entity: Option<PostV2PackagesError> = serde_json::from_str(&content).ok();
-        Err(Error::ResponseError(ResponseContent {
-            status,
-            content,
-            entity,
-        }))
-    }
-}
-
 /// Allocate a new party to the participant node
 pub async fn post_v2_parties(
     configuration: &configuration::Configuration,
